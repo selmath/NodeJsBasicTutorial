ad27cdaa9bec6cce552c9366b1ec9b7808d4cf7d
selmath <selmathmu2008@gmail.com>
2022-07-11 16:03:53 +0400

    Add files via upload

---
+++ 13TUT-MongoDB/middleware/verifyJWT.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const verifyJWT = (req, res, next) => {
+	const authHeader = req.headers.authorization || req.headers.Authorization;
+	if(!authHeader?.startsWith('Bearer ')) return res.sendStatus(401); // un-authorized
+	const accessToken = authHeader.split(' ')[1];
+	jwt.verify(
+		accessToken,
+		process.env.ACCESS_TOKEN_SECRET,
+		(err, decoded) => {
+			console.error(err);
+			if(err) return res.sendStatus(403); // Invalid Token
+			req.user = decoded.UserInfo.username;
+			req.roles = decoded.UserInfo.roles;
+			next();
+		}
+	);
+}
+
+module.exports = verifyJWT;
