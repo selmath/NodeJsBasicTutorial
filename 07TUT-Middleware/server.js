c3acffde9a88b6c03eae7d3bc8b2fbb944176b73
selmath <selmathmu2008@gmail.com>
2022-07-11 12:32:03 +0400

    Add files via upload

---
+++ 07TUT-Middleware/server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents')
+const errorHandler  = require('./middleware/errorHandler')
+const PORT = process.env.PORT || 3500;
+
+
+// custom MIDDLEWARE logger 
+
+/*app.use((req, res, next) => {
+	logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+	console.log(`${req.method}: ${req.path}`);
+	next();
+})*/
+app.use(logger);
+
+// add middle ware for CORS: Cross Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:', 'http://localhost:3500'];
+const corsOptions = {
+	origin: (origin, callback) => {		
+		if(whitelist.indexOf(origin) !== -1 || !origin){			// 
+			callback(null, true);
+		}else{
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// Adding built-in MIDDLEWARE to handle url encoded data
+// in other words form data
+// 'contentType - application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// Adding built-in MIDDLEWARE to json data
+app.use(express.json());
+
+// server static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Routing 
+app.get('^/$|/index(.html)?', (req, res) => {
+	//res.sendFile('./views/index.html', {root: __dirname} );
+	res.sendFile(path.join(__dirname, 'views' , 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views' , 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+	res.redirect(301, '/new-page.html');
+});
+
+// Route handlers - chaining routes
+
+app.get('/hello(.html)?', (req, res, next) => {
+	console.log('attempted to load hello.html');
+	next();
+}, (req, res) => {
+	res.send('Hello world');
+});
+
+// chaining router handlers
+
+const one = (req, res, next) => {
+	console.log('One');
+	next();
+}
+
+const two = (req, res, next) => {
+	console.log('two');
+	next();
+}
+
+const three = (req, res) => {
+	console.log('three');
+	res.send('Finished');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+//default
+/*app.get('/*', (req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'views' , '404.html'));
+});*/
+
+app.all('*', (req, res) => {
+	res.status(404);
+	if(req.accepts('html')){
+		res.sendFile(path.join(__dirname, 'views' , '404.html'));
+	}else if(req.accepts('json')){
+		res.json({error: '404 Not Found'});
+	}else{
+		res.type('txt').send('404 Not Found');
+	}
+});
+
+// middle ware for error handling for 500
+/*app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500).send(err.message);
+})*/
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Application running on port ${PORT} `));
