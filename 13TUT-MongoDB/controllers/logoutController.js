e499a8f854e0eb1335f0f070e0bfdd33e52b9b8c
selmath <selmathmu2008@gmail.com>
2022-07-11 16:01:58 +0400

    Add files via upload

---
+++ 13TUT-MongoDB/controllers/logoutController.js
@@ -0,0 +1,32 @@
+const usersDB = {
+	users: require('../model/users.json'),
+	setUsers: function (data) { this.users = data }
+}
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+	const cookies = req.cookies;
+	
+	if(!cookies?.jwt) return res.sendStatus(204); // No Content
+	const refreshToken = cookies.jwt;
+	
+	const foundUser = usersDB.users.find(usr => usr.refreshToken === refreshToken);
+	if(!foundUser){
+		res.clearCookie('jwt', {httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+		return res.sendStatus(204); // Forbidden
+	}
+	
+	// Delete refresh token
+	const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+	const currentUser = {...foundUser, refreshToken: ''};
+	usersDB.setUsers([...otherUsers, currentUser]);
+	await fsPromises.writeFile(
+		path.join(__dirname, '..', 'model', 'users.json'),
+		JSON.stringify(usersDB.users)
+	);
+	res.clearCookie('jwt', {httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // secure: true for production which will only server https
+	res.sendStatus(204);
+}
+
+module.exports = { handleLogout };
