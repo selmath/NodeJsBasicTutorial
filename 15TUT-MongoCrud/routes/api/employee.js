b278aefd2b5121f49c18cc4019a8dca317d20b1e
selmath <selmathmu2008@gmail.com>
2022-07-11 16:31:06 +0400

    Create employee.js

---
+++ 15TUT-MongoCrud/routes/api/employee.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const employeeController = require('../../controllers/employeeController');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
+
+router.route('/')
+	.get(employeeController.getAllEmployees)
+	.post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.createNewEmployee)
+	.put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.updateEmployee)
+	.delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployee);
+	
+router.route('/:id')
+	.get(employeeController.getEmployee);
+
+module.exports = router;
